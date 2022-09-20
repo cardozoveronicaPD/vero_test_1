2a407574e4f570d29eda4aa18cd776c0da13fb13
vcardozo <vcardozo@pagerduty.com>
2022-09-20 09:56:18 -0300

    new changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn develop
+          Learn develop now
         </a>
       </header>
     </div>
